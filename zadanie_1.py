04045db6f12a7785661a3cbad7e6b43504e2adb1
Dziomek <dziomek@student.agh.edu.pl>
2022-05-21 14:30:34 +0200

    typo

--- zadanie_1.py
+++ zadanie_1.py
@@ -10,7 +10,7 @@ Do zrealizowania zadania potrzebne będą biblioteki pyttsx3 i datetime
 Kroki, które należy podjąć:
 1) za pomocą biblioteki pyttsx3 należy zainicjować silnik, który będzie odpowiedzialny za zamianę tekstu na mowę.
 2) wczytać tekst przy użyciu funkcji input()
-3) uruchomić silnik i sprawić, by program mówił (łącznie w tym kroku wykorzystuje się dwie komendy obiektu engine)
+3) uruchomić silnik i sprawić, by program mówił (łącznie w tym kroku wykorzystuje się dwie metody obiektu engine)
 4) pobrać aktualną datę przy użyciu metody modułu datetime i dodaniu jej do outputu
 
 '''
